46f6454e067381ffce263331fb80e880ab9aa5e3
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2022-12-30 14:28:11 -0500

    add explicit findById()

--- src/main/java/com/example/repo/TourRepository.java
+++ src/main/java/com/example/repo/TourRepository.java
@@ -4,34 +4,35 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.example.domain.Tour;
 
+import java.util.Optional;
+
+@RepositoryRestResource(exported = false)
 public interface TourRepository extends PagingAndSortingRepository<Tour, Integer>{
   Page<Tour> findByTourPackageCode(@Param("code") String code, Pageable pageable);
 
-//  @Override
   @RestResource(exported = false)
   <S extends Tour> S save(S s);
 
-//  @Override
   @RestResource(exported = false)
   <S extends Tour> Iterable<S> saveAll(Iterable<S> iterable);
 
-//  @Override
   @RestResource(exported = false)
   void deleteById(Integer integer);
 
-//  @Override
   @RestResource(exported = false)
   void delete(Tour tour);
 
-//  @Override
   @RestResource(exported = false)
   void deleteAll(Iterable<? extends Tour> iterable);
 
-//  @Override
   @RestResource(exported = false)
   void deleteAll();
+
+  @RestResource(exported = false)
+  Optional<Tour> findById(int tourId);
 }
