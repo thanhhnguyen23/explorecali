aa7827025fb628815f37219766760d7a253939ab
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2022-12-30 12:54:54 -0500

    add custom port command

--- src/main/resources/scripts/build.sh
+++ src/main/resources/scripts/build.sh
@@ -3,5 +3,8 @@
 # build jar file
 mvn clean install &&
 
-# run java from cli
-java -jar target/*jar
+# run java from cli with default port (8080)
+java -jar target/*jar && 
+
+# run java from cli with custom port (8081)
+java -Dserver.port=9090 -jar target/*jar && 
